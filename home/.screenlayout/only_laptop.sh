68855406409654c0b4a34f53aeb22abe76868e5b
Ryan Harter <harterrt@gmail.com>
2017-03-31 15:23:53 -0400

    Adding default printer for lpq/lpr

--- home/.screenlayout/only_laptop.sh
+++ home/.screenlayout/only_laptop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1480 --rotate normal \
+xrandr --output eDP1 --primary --pos 0x1480 --rotate normal \
        --output HDMI2 --off
 ~/.screenlayout/set_touchpad.sh 1
