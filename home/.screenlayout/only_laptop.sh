27033cd835448084b28d1c0afcee2c7449cb7ba4
Ryan Harter <harterrt@gmail.com>
2017-09-08 07:50:19 -0400

    Update laptop resolution.

--- home/.screenlayout/only_laptop.sh
+++ home/.screenlayout/only_laptop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --pos 0x1480 --rotate normal \
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x1480 --rotate normal \
        --output HDMI2 --off
 ~/.screenlayout/set_touchpad.sh 1
